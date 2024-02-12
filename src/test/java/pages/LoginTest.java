87537e435da679f63f565ece10df3b190e068903
Tharun <kodurutharun18@gmail.com>
2024-02-12 14:09:17 +0530

    modify 5 jenkins

--- src/test/java/pages/LoginTest.java
+++ src/test/java/pages/LoginTest.java
@@ -7,11 +7,11 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class LoginTest {
     WebDriver driver;
-    final Logger LOGGER = Logger.getLogger(LoginTest.class.getName());
+   
 
     @BeforeClass
     public void setUp() {
@@ -25,23 +25,17 @@ public class LoginTest {
     public void testLoginSuccess() {
     	LoginPage Login = new LoginPage(driver);
         // Test login functionality with valid credentials
-        LOGGER.log(Level.INFO, "Navigating to login page...");
         driver.get("https://demowebshop.tricentis.com/login");
-        LOGGER.log(Level.INFO, "Entering username...");
         driver.findElement(Login.usernameField).sendKeys("username");
-        LOGGER.log(Level.INFO, "Entering password...");
         driver.findElement(Login.passwordField).sendKeys("password");
-        LOGGER.log(Level.INFO, "Clicking login button...");
         driver.findElement(Login.loginButton).click();
 
         // Add assertions for successful login
-        LOGGER.log(Level.INFO, "Logged in successfully.");
     }
 
     @AfterClass
     public void tearDown() {
         // Quit WebDriver
-        LOGGER.log(Level.INFO, "Quitting WebDriver...");
         driver.quit();
     }
 }
