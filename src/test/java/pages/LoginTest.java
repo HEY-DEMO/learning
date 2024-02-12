5254ac4a8041988c1fab5ecae3ddb52907719552
Tharun <kodurutharun18@gmail.com>
2024-02-12 14:11:01 +0530

    modify 6 jenkins

--- src/test/java/pages/LoginTest.java
+++ src/test/java/pages/LoginTest.java
@@ -2,11 +2,12 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.logging.Level;
+
 
 
 public class LoginTest {
@@ -16,9 +17,9 @@ public class LoginTest {
     @BeforeClass
     public void setUp() {
         // Set up WebDriver
-        System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
-        driver = new EdgeDriver();
-        driver.manage().window().maximize();
+    	 System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
+         EdgeOptions options = new EdgeOptions();
+         options.addArguments("--headless");
     }
 
     @Test
