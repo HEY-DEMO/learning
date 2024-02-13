92ae33193136ea8306fc152782ae0208fbbafd62
Tharun <kodurutharun18@gmail.com>
2024-02-13 16:06:05 +0530

    retry3

--- src/test/java/pages/LoginTest.java
+++ src/test/java/pages/LoginTest.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -18,14 +19,14 @@ public class LoginTest {
     @BeforeClass
     public void setUp() {
         // Set up WebDriver
-    	 System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
+    	 System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
   
     }
 
     @Test
     public void testLoginSuccess() {
  
-    	  driver = new EdgeDriver();
+    	  driver = new FirefoxDriver();
     	LoginPage Login = new LoginPage(driver);
         // Test login functionality with valid credentials
         driver.get("https://demowebshop.tricentis.com/login");
